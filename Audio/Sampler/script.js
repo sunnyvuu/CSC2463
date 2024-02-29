6e472bf5fb85ad5e64aaaa703e0d81f579865d7e
Madeline Yi <sunhunfunbun@gmail.com>
2024-02-29 14:40:28 -0600

    adjusted styling

--- Audio/Sampler/script.js
+++ Audio/Sampler/script.js
@@ -11,51 +11,44 @@ function preload() {
 }
 
 function setup() {
-  createCanvas(500, 500);
-  background("pink");
+  noCanvas();
+
+  let container = createDiv("");
+  container.id("container");
 
   // Initialize the pitch shift and connect it to the master output
   pitchShift = new Tone.PitchShift().toMaster();
 
-  // Create a slider for pitch control
+  // Create a slider for pitch control and attach event listener
   slider = createSlider(-7, 7, 1);
-  slider.position(19, 325);
+  slider.parent(container);
   slider.style("width", "400px");
+  slider.input(() => (pitchShift.pitch = slider.value())); // Update pitch on slider input
+
+  let buttonContainer = createDiv("");
+  buttonContainer.parent(container);
 
   // Create audio players and buttons for each cat sound
+  // Then set their parent to the button container
   cat1 = new Tone.Player("Cat1.wav").connect(pitchShift);
-  button_cat1 = createButton("");
-  button_cat1.position(19, 19);
-  button_cat1.mousePressed(() => cat1.start());
+  button_cat1 = createImgButton(imgCat1, cat1, buttonContainer);
 
   cat2 = new Tone.Player("Cat2.mp3").connect(pitchShift);
-  button_cat2 = createButton("");
-  button_cat2.position(19, 100);
-  button_cat2.mousePressed(() => cat2.start());
+  button_cat2 = createImgButton(imgCat2, cat2, buttonContainer);
 
   cat3 = new Tone.Player("Cat3.wav").connect(pitchShift);
-  button_cat3 = createButton("");
-  button_cat3.position(19, 181);
-  button_cat3.mousePressed(() => cat3.start());
+  button_cat3 = createImgButton(imgCat3, cat3, buttonContainer);
 
   cat4 = new Tone.Player("Cat4.wav").connect(pitchShift);
-  button_cat4 = createButton("");
-  button_cat4.position(19, 262);
-  button_cat4.mousePressed(() => cat4.start());
+  button_cat4 = createImgButton(imgCat4, cat4, buttonContainer);
+}
 
-  // Apply the images to the buttons
-  setButtonImage(button_cat1, imgCat1);
-  setButtonImage(button_cat2, imgCat2);
-  setButtonImage(button_cat3, imgCat3);
-  setButtonImage(button_cat4, imgCat4);
-
-  // Style the buttons
-  const buttons = [button_cat1, button_cat2, button_cat3, button_cat4];
-  buttons.forEach((button) => {
-    button.style("padding", "0px");
-    button.style("border", "none");
-    button.style("background-color", "transparent");
-  });
+function createImgButton(img, player, parentDiv) {
+  let button = createButton("");
+  button.mousePressed(() => player.start());
+  setButtonImage(button, img);
+  button.parent(parentDiv);
+  return button;
 }
 
 function setButtonImage(button, img) {
@@ -68,17 +61,8 @@ function setButtonImage(button, img) {
   button.style("background-image", "url(" + imgResized + ")");
   button.style("background-size", "50px 50px");
   button.style("background-repeat", "no-repeat");
+  button.style("padding", "5px");
+  button.style("border", "none");
+  button.style("background-color", "transparent");
   button.size(50, 50);
-}
-
-function draw() {
-  // Draw the text for the slider control
-  fill(0);
-  noStroke();
-  textSize(20);
-  textAlign(CENTER, CENTER);
-  text("Slider will shift pitch", width / 2, height / 2 + 50);
-
-  // Update the pitch shift value
-  pitchShift.pitch = slider.value();
 }
